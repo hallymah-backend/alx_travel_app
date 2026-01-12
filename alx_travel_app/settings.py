88157099024a75d22504b53107f664c5125c003f
hallymah-backend <olakitanhalimat@gmail.com>
2026-01-12 15:44:44 +0100

    Initial Django project setup with DRF, MySQL, and Swagger

--- alx_travel_app/settings.py
+++ alx_travel_app/settings.py
@@ -117,7 +117,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.mysql',
+        # 'ENGINE': 'django.db.backends.mysql',
         'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
